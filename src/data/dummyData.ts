2ebde03be0574f8c0d522d3485a22d4b1423b3c5
Sebastian Ingebrigtsen <sebastianingebrigtsen@users.noreply.github.com>
2026-03-15 22:26:13 +0100

    Endret dummy data til Formel 1

--- src/data/dummyData.ts
+++ src/data/dummyData.ts
@@ -1,53 +1,50 @@
 export const dummyItems = [
   {
     id: 1,
-    name: 'Tur',
-    description: 'En rolig tur med flott utsikt.',
-    image:
-      'https://media.gettyimages.com/id/1412545234/photo/feeling-of-freedom.jpg?s=612x612&w=gi&k=20&c=PhSxgLkWodRIOAUGpmd1tTaVRw9VlwBHwt3vsp-vF5g=',
+    name: 'Max Verstappen',
+    description: 'Red Bull Racing – 3x World Champion',
+    image: 'https://media.formula1.com/content/dam/fom-website/drivers/2024Drivers/verstappen.jpg.img.512.medium.jpg',
   },
   {
     id: 2,
-    name: 'Byvandring',
-    description: 'Oppdag nye steder og små kaféer i byen.',
-    image:
-      'https://media.istockphoto.com/id/1440731290/photo/woman-walking-with-modern-city-background.jpg?s=612x612&w=0&k=20&c=1VWdpZfu79ufLjrEAhEZ6ScLF28ooGRBOfD-AFmn_io=',
+    name: 'Lewis Hamilton',
+    description: 'Mercedes – 7x World Champion',
+    image: 'https://media.formula1.com/content/dam/fom-website/drivers/2024Drivers/hamilton.jpg.img.512.medium.jpg',
   },
   {
     id: 3,
-    name: 'Stranddag',
-    description: 'En avslappende dag ved sjøen.',
-    image: 'https://thumbs.dreamstime.com/b/happy-family-walking-beach-day-time-concept-friendly-46034803.jpg',
+    name: 'Charles Leclerc',
+    description: 'Ferrari – Monaco driver',
+    image: 'https://media.formula1.com/content/dam/fom-website/drivers/2024Drivers/leclerc.jpg.img.512.medium.jpg',
   },
   {
     id: 4,
-    name: 'Sykkeltur',
-    description: 'En aktiv dag med sykkel og natur.',
-    image:
-      'https://www.datocms-assets.com/55942/1739970170-20240716_184503-0-1.jpg?q=70&auto=format&w=1280&fit=crop&crop=faces,focalpoint&iptc=allow&h=721&fp-x=0.65&fp-y=0.46',
+    name: 'Lando Norris',
+    description: 'McLaren – British driver',
+    image: 'https://media.formula1.com/content/dam/fom-website/drivers/2024Drivers/norris.jpg.img.512.medium.jpg',
   },
   {
     id: 5,
-    name: 'Fjelltur',
-    description: 'Krevende tur til toppen med spektakulær utsikt.',
-    image: 'https://images.unsplash.com/photo-1464822759023-fed622ff2c3b?q=80&w=2070&auto=format&fit=crop',
+    name: 'Fernando Alonso',
+    description: 'Aston Martin – 2x World Champion',
+    image: 'https://media.formula1.com/content/dam/fom-website/drivers/2024Drivers/alonso.jpg.img.512.medium.jpg',
   },
   {
     id: 6,
-    name: 'Kafétur',
-    description: 'Nyt en god kopp kaffe og ferske bakervarer.',
-    image: 'https://images.unsplash.com/photo-1554118811-1e0d58224f24?q=80&w=2047&auto=format&fit=crop',
+    name: 'Oscar Piastri',
+    description: 'McLaren – Australian rising star',
+    image: 'https://media.formula1.com/content/dam/fom-website/drivers/2024Drivers/piastri.jpg.img.512.medium.jpg',
   },
   {
     id: 7,
-    name: 'Båttur',
-    description: 'En herlig dag på fjorden i strålende sol.',
-    image: 'https://images.unsplash.com/photo-1534447677768-be436bb09401?q=80&w=1980&auto=format&fit=crop',
+    name: 'George Russell',
+    description: 'Mercedes – British driver',
+    image: 'https://media.formula1.com/content/dam/fom-website/drivers/2024Drivers/russell.jpg.img.512.medium.jpg',
   },
   {
     id: 8,
-    name: 'Telttur',
-    description: 'Sov under åpen himmel og våkn til fuglesang.',
-    image: 'https://images.unsplash.com/photo-1478131143081-80f7f84ca84d?q=80&w=2070&auto=format&fit=crop',
+    name: 'Carlos Sainz',
+    description: 'Ferrari – Spanish driver',
+    image: 'https://media.formula1.com/content/dam/fom-website/drivers/2024Drivers/sainz.jpg.img.512.medium.jpg',
   }
 ];
